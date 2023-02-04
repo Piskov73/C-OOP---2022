46700ca43f6b5c0d3b8c19c194a48abecf6be236
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-04 19:19:53 +0200

    Add files via upload

---
+++ 03 Inheritance/05. Stack of Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stak = new StackOfStrings();
+            string[] strings= new string[] {"1", "2","3","4"};
+
+            Console.WriteLine(stak.IsEmpty());
+            stak.AddRange(strings);
+            Console.WriteLine(stak.IsEmpty());
+            Console.WriteLine(string.Join(", ",stak));
+        }
+    }
+}
---
+++ 03 Inheritance/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomStack
+{
+    internal class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return Count== 0;
+        }
+
+        public StackOfStrings AddRange(IEnumerable<string> strings)
+        {
+            foreach (string s in strings)
+            { 
+
+                this.Push(s);
+
+            }
+            return this;
+        }
+    }
+}
