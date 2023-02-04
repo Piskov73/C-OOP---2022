12a8ef5afbf11b72b4c1d94391dce1ec981ab84f
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-04 18:57:40 +0200

    Add files via upload

---
+++ 03 Inheritance/04. Random List/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList strings= new RandomList() {"1","2","9","oiu"};   
+            for (int i = 0;i <4; i++) 
+            {
+                Console.WriteLine(strings.RandomString());
+            }
+           
+        }
+    }
+}
