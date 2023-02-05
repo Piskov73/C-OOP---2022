c4d76ac89fe2cb81e75d278cfa0ff8dfdc0bb3cf
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-05 18:17:57 +0200

    Add files via upload

---
+++ 04 Inheritance - Exercise/01. Person/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name=Console.ReadLine();
+            int age =int.Parse(Console.ReadLine());
+            if (age < 16)
+            {
+                var child=new Child(name,age);
+                Console.WriteLine(child.ToString());
+            }
+            else
+            {
+                var person =new Person(name,age);
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
