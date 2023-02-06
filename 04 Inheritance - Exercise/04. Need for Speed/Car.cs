f018877dcbb8f2ad76f75ff0a6036c54bd4b3462
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-06 12:20:41 +0200

    Add files via upload

---
+++ 04 Inheritance - Exercise/04. Need for Speed/Car.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace NeedForSpeed
+{
+    public class Car : Vehicle
+    {
+        
+        private const double DEFAUT_FUEL_CAR_COMSUMPTION = 3;
+        public Car(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+            
+        }
+        public override double FuelConsumption => DEFAUT_FUEL_CAR_COMSUMPTION;
+    }
+}
---
+++ 04 Inheritance - Exercise/04. Need for Speed/SportCar.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+       
+        private const double DEFAUT_SPORT_CAR_FUEL_CONSUMPTION = 10;
+        public SportCar(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+        public override double FuelConsumption => DEFAUT_SPORT_CAR_FUEL_CONSUMPTION;
+    }
+}
