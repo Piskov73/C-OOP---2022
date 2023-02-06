d326c6e6944bdc8267ee0dc7d8cff38a44c78323
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-06 10:43:17 +0200

    Add files via upload

---
+++ 04 Inheritance - Exercise/03. Players and Monsters/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var hero =new Hero("Games",1);
+            Console.WriteLine(hero);
+            var elf = new Elf("Snega", 23);
+            Console.WriteLine(elf);
+            var museElf = new MuseElf("Lusi", 43);
+                Console.WriteLine(museElf);
+            var wizard = new Wizard("Albena", 44);
+            Console.WriteLine(wizard);
+            var darkWizard = new DarkWizard("Mario", 43);
+            Console.WriteLine(darkWizard);
+            var soulMaster = new SoulMaster("Ivan4o", 32);
+            Console.WriteLine(soulMaster);
+            var knight = new Knight("Stamat", 55);
+            Console.WriteLine(knight);
+            var darkKnight = new DarkKnight("Petrun4o", 19);
+            Console.WriteLine(darkKnight);
+            var bladeKnight = new BladeKnight("Boris4o", 44);
+            Console.WriteLine(bladeKnight);
+        }
+    }
+}
