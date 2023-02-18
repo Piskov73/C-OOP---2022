cf619619dd44ce92754c9b9fa22d33bc7eedc1d4
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-18 13:31:06 +0200

    Add files via upload

---
+++ 05 Encapsulation - Lab/02. Salary/Person.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+       
+        private string firstName;
+
+        private string lastName;
+
+        private int age;
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set { firstName = value; }
+        }
+
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set { lastName = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set { age = value; }
+        }
+        public decimal Salary { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+        }
+
+
+
+    }
+}
