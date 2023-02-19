97bd07c0860eeb7824a8d5cd901356863edc87d4
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-19 20:51:28 +0200

    Add files via upload

---
+++ 06 Encapsulation - Exercise/01. Class Box Data/ExceptionMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace _01ClassBoxData
+{
+    public static class ExceptionMessage
+    {
+        public const string CANNOT_ZERO_NEGATIVE_NUMBER = "{0} cannot be zero or negative.";
+    }
+}
---
+++ 06 Encapsulation - Exercise/01. Class Box Data/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace _01ClassBoxData
+{
+    using System;
+    using System.Threading.Channels;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                Box box=new Box(length, width, height);
+                Console.WriteLine(box);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
