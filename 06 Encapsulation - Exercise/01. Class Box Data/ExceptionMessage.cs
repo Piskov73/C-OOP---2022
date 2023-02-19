ac86241e514d66399913d34efc62aa0885f4661a
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-02-19 21:30:37 +0200

    Delete ExceptionMessage.cs

--- 06 Encapsulation - Exercise/01. Class Box Data/ExceptionMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _01ClassBoxData
-{
-    public static class ExceptionMessage
-    {
-        public const string CANNOT_ZERO_NEGATIVE_NUMBER = "{0} cannot be zero or negative.";
-    }
-}
