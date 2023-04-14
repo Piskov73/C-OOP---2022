db77f7097f2f8b92c5c850e0c5ca79d51cad12e7
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-04-14 18:49:19 +0300

    Add files via upload

---
+++ 09 Polymorphism-Lab/02. Animals/Dog.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Animals
+{
+    using System.Text;
+
+    public class Dog : Animal
+    {
+        public Dog(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(base.ExplainSelf());
+            sb.AppendLine("DJAAF");
+            return sb.ToString().Trim();
+        }
+    }
+}
