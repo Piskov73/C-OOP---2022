9ed0de617b65a54d0253998a8f08135e28732d89
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-05-07 09:28:01 +0300

    Delete MessageException.cs

--- 06 Encapsulation - Exercise/03. Shopping Spree/MessageException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _3ShoppingSpree
-{
-    public static class MessageException
-    {
-        public const string CANNOT_BE_EMPTY = "Name cannot be empty";
-
-        public const string CANNOT_BE_NEGATIVE = "Money cannot be negative";
-    }
-}
