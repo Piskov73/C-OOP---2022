aadc57249eadebf1d55df1862919f0d0ca952182
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-06-04 12:41:20 +0300

    Add files via upload

---
+++ 09 Polymorphism-Lab/03Shapes/StartUp.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Shapes
+{
+    using System;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Shape shape = null;
+            List<Shape> shapes = new List<Shape>();
+            shape = new Rectangle(5, 12);
+            shapes.Add(shape);
+            shape = new Circle(47);
+            shapes.Add(shape);
+            foreach (var item in shapes)
+            {
+                Console.WriteLine(item.CalculateArea());
+                Console.WriteLine(item.CalculatePerimeter());
+                Console.WriteLine(item.Draw());
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
