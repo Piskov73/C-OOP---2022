df031e25e3528099790759c88ee21b4493d45896
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-06-10 12:43:11 +0300

    Delete 10 PolymorphismExercise/01Vehicles/Factories directory

--- 10 PolymorphismExercise/01Vehicles/Factories/Interface/IVehiclesFactories.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Vehicles.Factories.Interface
-{
-using Models.Interface;
-    public interface IVehiclesFactories
-    {
-        IVehicle CreateVehicle(string type, double fuelQuantity,double fuelConsumption);
-    }
-}
