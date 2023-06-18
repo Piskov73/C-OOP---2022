f505f67674dfb45326c79e44dae3dbb30bb23306
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-06-18 12:26:53 +0300

    Add files via upload

---
+++ 11ExceptionsAndErrorHandlingLab/01SquareRoot/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace _01SquareRoot
+{
+    using System;
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            try
+            {
+                SquareRoot(number);
+            }
+            catch (ArgumentException ae)
+            {
+
+                Console.WriteLine(ae.Message);
+            }
+            Console.WriteLine("Goodbye.");
+        }
+
+        static void SquareRoot(double number)
+        {
+            if(number < 0)
+            {
+                throw new ArgumentException("Invalid number.");
+            }
+            Console.WriteLine($"{Math.Sqrt(number)}");
+        }
+    }
+}
---
+++ 11ExceptionsAndErrorHandlingLab/02EnterNumbers/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace EnterNumbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(", ",ReadNumber()));
+        }
+       private static List<int> ReadNumber()
+        {
+            List<int> numbersCollection = new List<int>();
+            int count = 1;
+            while (numbersCollection.Count < 10)
+            {
+                try
+                {
+                    int numb = int.Parse(Console.ReadLine());
+                    if (numb <= count || numb >= 100)
+                    {
+                        throw new ArgumentException($"Your number is not in range {count} - 100!");
+                    }
+                    count=numb;
+                    numbersCollection.Add(numb);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch(ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+
+            }
+            return numbersCollection;
+        }
+    }
+}
