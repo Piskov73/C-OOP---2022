e262f69420ca0ef471a53900a8d223c0b9d87a71
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-07-18 18:07:10 +0300

    Add files via upload

---
+++ 15ReflectionAndAttributes/03 MissionPrivateImpossible/Spy.cs
@@ -0,0 +1,29 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Reflection;
+    using System.Text;
+    public class Spy
+    {
+        public Spy()
+        {
+
+        }
+        public string RevealPrivateMethods(string investigationClass)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type classType = Type.GetType(investigationClass);
+            MethodInfo[] methodInfos = classType.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+            sb.AppendLine($"All Private Methods of Class: {investigationClass}")
+                .AppendLine($"Base Class: {classType.BaseType.Name}");
+            foreach ( MethodInfo methodInfo in methodInfos )
+            {
+                sb.AppendLine(methodInfo.Name );
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
