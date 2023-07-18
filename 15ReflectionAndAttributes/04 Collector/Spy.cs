e77fb3b611913317d0dfbcc1014ea6a57478f11b
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-07-18 18:34:02 +0300

    Add files via upload

---
+++ 15ReflectionAndAttributes/04 Collector/Spy.cs
@@ -0,0 +1,32 @@
+﻿namespace Stealer
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    public class Spy
+    {
+        public Spy()
+        {
+
+        }
+        public string CollecktorGetersAndSeters(string inspectionClass)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            Type typeClass = Type.GetType(inspectionClass);
+
+            MethodInfo[] methodsInfo = typeClass.GetMethods(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            foreach (MethodInfo method in methodsInfo.Where(m => m.Name.StartsWith("get")))
+            {
+                sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+            }
+            foreach (MethodInfo method in methodsInfo.Where(m => m.Name.StartsWith("set")))
+            {
+                sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
