1577f2d72b75f346c9bca77cce9dc226fd2d7a14
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-10-14 14:38:25 +0300

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Exam Regular - 05 August 2023/02. Business Logic/Models/Bank.cs
@@ -0,0 +1,99 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BankLoan.Models
+{
+    public abstract class Bank : IBank
+    {
+        private string name;
+        private int capacity;
+        private List<ILoan> loans;
+        private List<IClient> clients;
+        public Bank(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.loans = new List<ILoan>();
+            this.clients = new List<IClient>();
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.BankNameNullOrWhiteSpace));
+                }
+                name = value;
+            }
+        }
+
+
+        public int Capacity
+        {
+            get { return capacity; }
+            private set { capacity = value; }
+        }
+
+
+
+        public IReadOnlyCollection<ILoan> Loans => this.loans.AsReadOnly();
+
+        public IReadOnlyCollection<IClient> Clients => this.clients.AsReadOnly();
+        public double SumRates()
+        {
+            return 1.0 * this.Loans.Sum(l => l.InterestRate);
+        }
+        public void AddClient(IClient Client)
+        {
+            if (this.Clients.Count == this.Capacity)
+            {
+                throw new ArgumentException(string.Format(ExceptionMessages.NotEnoughCapacity));
+            }
+
+            this.clients.Add(Client);
+        }
+        public void RemoveClient(IClient Client)
+        {
+           this.clients.Remove(Client);
+        }
+
+        public void AddLoan(ILoan loan)
+        {
+            this.loans.Add(loan);
+        }
+
+        public string GetStatistics()
+        {
+            string name = "";
+            if(this.Clients.Count == 0)
+            {
+                name = "none";
+            }
+            else
+            {
+               List<string> names=this.Clients.Select(c => c.Name).ToList();
+                name= string.Join(", ", names);
+            }
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Name: {Name}, Type: {this.GetType().Name}")
+                .AppendLine($"Clients: {name}")
+                .AppendLine($"Loans: {this.Loans.Count}, Sum of Rates: {SumRates()}");
+
+
+            return sb.ToString().TrimEnd();
+        }
+
+       
+
+
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Exam Regular - 05 August 2023/02. Business Logic/Repositories/LoanRepository.cs
@@ -0,0 +1,33 @@
+﻿using BankLoan.Models.Contracts;
+using BankLoan.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BankLoan.Repositories
+{
+    public class LoanRepository : IRepository<ILoan>
+    {
+        private List<ILoan> loanList;
+
+        public LoanRepository()
+        {
+            this.loanList = new List<ILoan>();
+        }
+        public IReadOnlyCollection<ILoan> Models => this.loanList.AsReadOnly();
+
+        public void AddModel(ILoan model)
+        {
+            this.loanList.Add(model);
+        }
+
+        public ILoan FirstModel(string name)
+        {
+        return this.loanList.FirstOrDefault(l=>l.GetType().Name == name);
+        }
+
+        public bool RemoveModel(ILoan model)
+        {
+           return this.loanList.Remove(model);
+        }
+    }
+}
