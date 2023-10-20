04628ff845f76d543625c5bf2b9fde87ae4493e5
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-10-20 16:45:23 +0300

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Retake Exam – 18 April 2023/01. Structure/Models/User.cs
@@ -0,0 +1,96 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Utilities.Messages;
+using System;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string firstName;
+        private string lastName;
+        private double rating;
+        private string drivingLicenseNumber;
+        private bool isBlocked;
+
+        public User(string firstName, string lastName, string drivingLicenseNumber)
+        { 
+            FirstName=firstName;
+            LastName=lastName;
+            DrivingLicenseNumber=drivingLicenseNumber;
+            this.rating=0;
+            this.isBlocked = false;
+
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+           private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.FirstNameNull));
+                }
+                firstName = value; 
+            }
+        }
+
+
+
+        public string LastName
+        {
+            get { return lastName; }
+           private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.LastNameNull));
+                }
+                lastName = value;
+            }
+        }
+
+
+        public double Rating { get => rating; private set => rating = value; }
+
+
+        public string DrivingLicenseNumber
+        {
+            get { return drivingLicenseNumber; }
+           private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.DrivingLicenseRequired));
+                }
+                drivingLicenseNumber = value; 
+            }
+        }
+
+
+        public bool IsBlocked { get => isBlocked;private set=>isBlocked = value; }
+
+        public void DecreaseRating()
+        {
+            this.rating -= 2;
+            if (this.rating <= 0)
+            {
+                this.rating = 0;
+                this.isBlocked = true;
+            }
+        }
+
+        public void IncreaseRating()
+        {
+            this.rating += 0.5;
+            if (this.rating >10)
+            {
+                this.rating=10;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} Driving license: {DrivingLicenseNumber} Rating: {Rating}";
+        }
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Retake Exam – 18 April 2023/01. Structure/Repositories/VehicleRepository.cs
@@ -0,0 +1,37 @@
+﻿using EDriveRent.Models.Contracts;
+using EDriveRent.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EDriveRent.Repositories
+{
+    public class VehicleRepository : IRepository<IVehicle>
+    {
+        private List<IVehicle> vehicles;
+        public VehicleRepository()
+        {
+            this.vehicles = new List<IVehicle>();
+        }
+        public IReadOnlyCollection<IVehicle> GetAll() => this.vehicles.AsReadOnly();
+        
+
+        public void AddModel(IVehicle model)
+        {
+            this.vehicles.Add(model);
+        }
+
+        public IVehicle FindById(string identifier)
+        {
+           return this.vehicles.FirstOrDefault(x=>x.LicensePlateNumber == identifier);
+        }
+
+      
+
+        public bool RemoveById(string identifier)
+        {
+            return this.vehicles.Remove(FindById(identifier));
+        }
+    }
+}
