4e15bc79389e827fcb68d87dec77a3323aa8f511
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-10-29 16:23:58 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Retake Exam – 19 December 2022/01. Structure/Models/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Utilities.Messages;
+
+namespace UniversityCompetition.Models
+{
+    public class Student : IStudent
+    {
+        private int id;
+        private string firstName;
+        private string lastName;
+        private List<int> coveredExams;
+        private IUniversity university;
+        public Student(int studentId, string firstName, string lastName)
+        {
+            this.Id= studentId;
+            this.FirstName= firstName;
+            this.LastName= lastName;
+            this.coveredExams= new List<int>();
+        }
+
+        public int Id { get => id; private set => id = value; }
+
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+                lastName = value;
+            }
+        }
+
+
+
+        public IReadOnlyCollection<int> CoveredExams => this.coveredExams.AsReadOnly();
+
+        public IUniversity University { get => university;private set =>university= value; }
+
+        public void CoverExam(ISubject subject)
+        {
+            this.coveredExams.Add(subject.Id);
+        }
+
+        public void JoinUniversity(IUniversity university)
+        {
+            this.university=university;
+        }
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Retake Exam – 19 December 2022/01. Structure/Models/Subject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Utilities.Messages;
+
+namespace UniversityCompetition.Models
+{
+    public abstract class Subject : ISubject
+    {
+        private int id;
+        private string name;
+        private double rate;
+        public Subject(int subjectId, string subjectName, double subjectRate)
+        { 
+            this.Id = subjectId;
+            this.Name = subjectName;
+            this.Rate = subjectRate;
+
+        }
+        public int Id { get =>id;private set => id = value; }
+
+        public string Name
+        {
+            get { return name; }
+           private set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+                name = value;
+            }
+        }
+
+
+        public double Rate { get => rate;private set=> rate = value; }
+    }
+}
