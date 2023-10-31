6f1c5c8f45502e22335d827a76cb1e370c1887da
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-10-31 19:12:25 +0200

    Add files via upload
    2 task 141/150

---
+++ 19 C# OOP Exam/C# OOP Retake Exam – 19 December 2022/02. Business Logic/IO/FIleWriter.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using UniversityCompetition.IO.Contracts;
+
+namespace UniversityCompetition.IO
+{
+    public class FIleWriter : IWriter
+    {
+        public FIleWriter() 
+        {
+            using (StreamWriter sw = new StreamWriter("../../../output.txt", false))
+            {
+                sw.Write("");
+            }
+        }
+        public void Write(string message)
+        {
+            using(StreamWriter sw=new StreamWriter("../../../output.txt",true))
+            {
+                sw.Write(message);
+            }
+        }
+
+        public void WriteLine(string message)
+        {
+            using (StreamWriter sw = new StreamWriter("../../../output.txt", true))
+            {
+                sw.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Retake Exam – 19 December 2022/02. Business Logic/Repositories/StudentRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    internal class StudentRepository : IRepository<IStudent>
+    {
+        private List<IStudent> students;
+
+        public StudentRepository()
+        {
+            this.students = new List<IStudent>();
+        }
+        public IReadOnlyCollection<IStudent> Models => this.students.AsReadOnly();
+
+        public void AddModel(IStudent model)
+        {
+            this.students.Add(model);
+        }
+
+        public IStudent FindById(int id)
+        {
+           return Models.FirstOrDefault(s=>s.Id==id);   
+        }
+
+        public IStudent FindByName(string name)
+        {
+            var studentName=name.Split().ToArray();
+            string firstName = studentName[0];
+            string lastName = studentName[1];
+            return Models.FirstOrDefault(s=>s.FirstName==firstName && s.LastName==lastName);
+        }
+    }
+}
