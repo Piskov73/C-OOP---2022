a659929b17f869e875217398b5d5da28a1984dd9
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-11-04 09:23:02 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Regular Exam - 10 December 2022/01. Structure/Repositories/DelicacyRepository.cs
@@ -0,0 +1,24 @@
+﻿using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Repositories
+{
+    public class DelicacyRepository : IRepository<IDelicacy>
+    {
+        private List<IDelicacy> items;
+
+        public DelicacyRepository()
+        {
+            this.items = new List<IDelicacy>();
+        }
+        public IReadOnlyCollection<IDelicacy> Models => this.items.AsReadOnly();
+
+        public void AddModel(IDelicacy model)
+        {
+            this.items.Add(model);
+        }
+    }
+}
