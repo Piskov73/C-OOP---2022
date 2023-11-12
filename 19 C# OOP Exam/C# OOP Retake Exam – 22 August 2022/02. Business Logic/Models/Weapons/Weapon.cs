15bfd33b8dfad28c00ee1a949c5a732736acc28c
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-11-12 08:29:02 +0200

    2 task 141/150
    2 task 141/150

---
+++ 19 C# OOP Exam/C# OOP Retake Exam – 22 August 2022/02. Business Logic/Models/Weapons/Weapon.cs
@@ -0,0 +1,41 @@
+﻿using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetWars.Models.Weapons
+{
+    public abstract class Weapon : IWeapon
+    {
+        private int destructionLevel;
+
+        private double price;
+        public Weapon(int destructionLevel, double price)
+        {
+            DestructionLevel = destructionLevel;
+            Price=price;
+        }
+
+        public double Price { get => price;private set => price = value; }
+
+
+        public int DestructionLevel
+        {
+            get { return destructionLevel; }
+           private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.TooLowDestructionLevel));
+                }
+                if (value > 10)
+                {
+                    throw new ArgumentException(string.Format(ExceptionMessages.TooHighDestructionLevel));
+                }
+                destructionLevel = value;
+            }
+        }
+
+    }
+}
