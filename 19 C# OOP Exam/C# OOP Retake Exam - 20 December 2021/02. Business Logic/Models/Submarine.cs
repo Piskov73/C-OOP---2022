8e4811b1abe5046ad40a2fb8cd9f01ae88033f4c
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-11-22 13:17:19 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Retake Exam - 20 December 2021/02. Business Logic/Models/Submarine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public class Submarine : Vessel
+    {
+        private const double armorThickness = 200;
+        private bool submergeMode;
+        public Submarine(string name, double mainWeaponCaliber, double speed)
+            : base(name, mainWeaponCaliber, speed, armorThickness)
+        {
+
+        }
+        public bool SubmergeMode => submergeMode;
+        public void ToggleSubmergeMode()
+        {
+            submergeMode = !submergeMode;
+            if (SubmergeMode)
+            {
+                MainWeaponCaliber += 40;
+                Speed -= 4;
+            }
+            else
+            {
+                MainWeaponCaliber -= 40;
+                Speed += 4;
+            }
+        }
+        public override void RepairVessel()
+        {
+            ArmorThickness = armorThickness;
+        }
+        public override string ToString()
+        {
+            string poisitionSubmerge = SubmergeMode ? "ON" : "OFF";
+            return base.ToString()+Environment.NewLine+ $" *Submerge mode: {poisitionSubmerge}";
+        }
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Retake Exam - 20 December 2021/02. Business Logic/Models/Vessel.cs
@@ -0,0 +1,107 @@
+﻿using NavalVessels.Models.Contracts;
+using NavalVessels.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NavalVessels.Models
+{
+    public abstract class Vessel : IVessel
+    {
+        private string name;
+        private ICaptain captain;
+        private double armorThickness;
+        private double mainWeaponCaliber;
+        private double speed;
+        private List<string> targets;
+        public Vessel(string name, double mainWeaponCaliber, double speed, double armorThickness)
+        {
+            Name = name;
+            MainWeaponCaliber=mainWeaponCaliber;
+            Speed=speed;
+            ArmorThickness=armorThickness;
+            this.targets=new List<string>();
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(string.Format(ExceptionMessages.InvalidVesselName));
+                }
+                name = value;
+            }
+        }
+
+        public ICaptain Captain
+        {
+            get => captain;
+            set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException(string.Format(ExceptionMessages.InvalidCaptainToVessel));
+                }
+                captain = value;
+            }
+        }
+        public double ArmorThickness
+        {
+            get => armorThickness;
+            set
+            {
+                if (value < 0)
+                    value = 0;
+
+                armorThickness = value;
+            }
+
+        }
+
+        public double MainWeaponCaliber { get => mainWeaponCaliber; protected set => mainWeaponCaliber = value; }
+
+        public double Speed { get => speed; protected set => speed = value; }
+
+        public ICollection<string> Targets => targets.AsReadOnly();
+
+        public void Attack(IVessel target)
+        {
+            if (target == null)
+                throw new NullReferenceException(string.Format(ExceptionMessages.InvalidTarget));
+
+            target.ArmorThickness -= MainWeaponCaliber;
+            this.targets.Add(target.Name);
+        }
+
+        public virtual void RepairVessel()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            string target=string.Empty;
+            if(Targets.Count == 0)
+            {
+                target = "None";
+            }
+            else
+            {
+                target=string.Join(", ",Targets);
+            }
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"- {Name}")
+                .AppendLine($" *Type: {this.GetType().Name}")
+                .AppendLine($" *Armor thickness: {ArmorThickness}")
+                .AppendLine($" *Main weapon caliber: {MainWeaponCaliber}")
+                .AppendLine($" *Speed: {Speed} knots")
+                .AppendLine($" *Targets: {target}");
+
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
