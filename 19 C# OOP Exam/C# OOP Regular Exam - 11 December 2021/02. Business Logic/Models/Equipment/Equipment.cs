7334de60fd5537e9a135836b59a8ebef9f0c82d5
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-11-23 16:58:45 +0200

    Add files via upload
    2 task 143/150

---
+++ 19 C# OOP Exam/C# OOP Regular Exam - 11 December 2021/02. Business Logic/Models/Equipment/Equipment.cs
@@ -0,0 +1,18 @@
+﻿using Gym.Models.Equipment.Contracts;
+
+namespace Gym.Models.Equipment
+{
+    public abstract class Equipment : IEquipment
+    {
+        private double weight;
+        private decimal price;
+        public Equipment(double weight, decimal price)
+        {
+            this.weight = weight;
+            this.price = price;
+        }
+        public double Weight => this.weight;
+
+        public decimal Price => this.price;
+    }
+}
