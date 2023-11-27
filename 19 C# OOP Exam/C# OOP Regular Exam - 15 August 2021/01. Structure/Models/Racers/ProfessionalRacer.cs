f45bcf8a882796085d3c41d1d2ff3d01e858d918
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-11-27 14:07:19 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Regular Exam - 15 August 2021/01. Structure/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,22 @@
+﻿using CarRacing.Models.Cars.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Racers
+{
+    public class ProfessionalRacer : Racer
+    {
+        private const string RacingBehavior = "strict";
+        private const int DrivingExperience = 30;
+        public ProfessionalRacer(string username, ICar car)
+            : base(username, RacingBehavior, DrivingExperience, car)
+        {
+        }
+        public override void Race()
+        {
+            base.Race();
+            base.DrivingExperience += 10;
+        }
+    }
+}
