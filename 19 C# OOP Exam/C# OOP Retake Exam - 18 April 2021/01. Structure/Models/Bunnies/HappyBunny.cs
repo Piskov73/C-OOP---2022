a53c8d845563e4a8038f40bda0dfbfe64a3308a8
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-11-30 18:10:40 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Retake Exam - 18 April 2021/01. Structure/Models/Bunnies/HappyBunny.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Bunnies
+{
+    public class HappyBunny : Bunny
+    {
+        private const int ENERGY = 100;
+        public HappyBunny(string name)
+            : base(name, ENERGY)
+        {
+        }
+
+        public override void Work()
+        {
+            base.Energy -= 10;
+        }
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Retake Exam - 18 April 2021/01. Structure/Models/Bunnies/SleepyBunny.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Bunnies
+{
+    public class SleepyBunny : Bunny
+    {
+        private const int ENERGY = 100;
+        public SleepyBunny(string name)
+            : base(name, ENERGY)
+        {
+        }
+
+        public override void Work()
+        {
+            base.Energy -= 15;
+        }
+    }
+}
