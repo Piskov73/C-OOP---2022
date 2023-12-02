43b66689e2881d980ee52e2c0d3a2e3043309fbf
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-12-02 18:59:59 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Regular Exam - 10 April 2021/02. Business Logic/Core/Controller.cs
@@ -0,0 +1,148 @@
+﻿using AquaShop.Core.Contracts;
+using AquaShop.Models.Aquariums;
+using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Repositories;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace AquaShop.Core
+{
+    public class Controller : IController
+    {
+        //        •	decorations - DecorationRepository 
+        //•	aquariums - collection of IAquarium
+
+        private DecorationRepository decorations;
+        private List<IAquarium> aquariums;
+
+        public Controller()
+        {
+            this .decorations = new DecorationRepository();
+            this.aquariums= new List<IAquarium>();
+        }
+        public string AddAquarium(string aquariumType, string aquariumName)
+        {
+            IAquarium aquarium;
+
+            if (aquariumType == nameof(FreshwaterAquarium))
+            {
+                aquarium = new FreshwaterAquarium(aquariumName);
+
+            }
+            else if (aquariumType == nameof(SaltwaterAquarium))
+            {
+                aquarium = new SaltwaterAquarium(aquariumName);
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidAquariumType));
+            }
+
+            this.aquariums.Add(aquarium);
+            return string.Format(OutputMessages.SuccessfullyAdded, aquariumType);
+        }
+
+        public string AddDecoration(string decorationType)
+        {
+            IDecoration decoration;
+
+            if (decorationType == nameof(Ornament))
+            {
+                decoration=new Ornament();
+            }
+            else if(decorationType == nameof(Plant))
+            {
+                decoration=new Plant();
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidDecorationType));
+            }
+
+            this.decorations.Add(decoration);
+            return string.Format(OutputMessages.SuccessfullyAdded,decorationType);
+        }
+        public string InsertDecoration(string aquariumName, string decorationType)
+        {
+            var aquarium=this.aquariums.FirstOrDefault(a=>a.Name==aquariumName);
+            var decoration = this.decorations.FindByType(decorationType);
+            if (decoration == null)
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InexistentDecoration, decorationType));
+
+            aquarium.AddDecoration(decoration);
+
+            this.decorations.Remove(decoration);
+
+            return string.Format(OutputMessages.EntityAddedToAquarium, decorationType,aquariumName);
+
+        }
+
+        public string AddFish(string aquariumName, string fishType, string fishName, string fishSpecies, decimal price)
+        {
+            var aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+            IFish fish;
+            if (fishType == nameof(FreshwaterFish))
+            {
+                fish=new FreshwaterFish(fishName, fishSpecies, price);
+            }
+            else if(fishType==nameof(SaltwaterFish))
+            {
+                fish =new SaltwaterFish(fishName, fishSpecies, price);
+            }
+            else
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InvalidFishType));
+            }
+            if(fish.GetType().Name==nameof(FreshwaterFish)&&aquarium.GetType().Name==nameof(SaltwaterAquarium)
+                || fish.GetType().Name == nameof(SaltwaterFish) && aquarium.GetType().Name == nameof(FreshwaterAquarium))
+            {
+                return string.Format(OutputMessages.UnsuitableWater);
+            }
+
+            aquarium.AddFish(fish);
+
+            return string.Format(OutputMessages.EntityAddedToAquarium,fishType,aquariumName);
+        }
+        public string FeedFish(string aquariumName)
+        {
+           var aquarium=this.aquariums.FirstOrDefault(a=>a.Name==aquariumName);
+
+            aquarium.Feed();
+
+            return string.Format(OutputMessages.FishFed,aquarium.Fish.Count);
+        }
+
+        public string CalculateValue(string aquariumName)
+        {
+            var aquarium = this.aquariums.FirstOrDefault(a => a.Name == aquariumName);
+            decimal priceAquarium=aquarium.Fish.Sum(p=>p.Price)+aquarium.Decorations.Sum(p=>p.Price);
+            priceAquarium = Math.Round(priceAquarium, 2);
+
+            return string.Format(OutputMessages.AquariumValue,aquariumName,priceAquarium);
+        }
+
+
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var aqu in this.aquariums)
+            {
+                sb.AppendLine(aqu.GetInfo());
+            } 
+              
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Regular Exam - 10 April 2021/02. Business Logic/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,99 @@
+﻿using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;
+        private int capacity;
+        private List<IDecoration> decorations;
+        private List<IFish> fish;
+
+        public Aquarium(string name, int capacity)
+        {
+            Name = name;
+            this.capacity = capacity;
+            this .decorations = new List<IDecoration>();
+            this .fish = new List<IFish>();
+
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidAquariumName));
+
+                name = value;
+            }
+        }
+
+
+        public int Capacity => this.capacity;
+
+        public int Comfort => Decorations.Sum(d=>d.Comfort);
+
+        public ICollection<IDecoration> Decorations => this.decorations.AsReadOnly();
+
+        public ICollection<IFish> Fish => this.fish.AsReadOnly();
+
+        public void AddFish(IFish fish)
+        {
+            if (Fish.Count == Capacity)
+                throw new InvalidOperationException(string.Format(ExceptionMessages.NotEnoughCapacity));
+
+            this.fish.Add(fish);
+        }
+        public bool RemoveFish(IFish fish)
+        {
+           return this.fish.Remove(fish);
+        }
+
+        public void AddDecoration(IDecoration decoration)
+        {
+           this.decorations.Add(decoration);
+        }
+
+
+        public void Feed()
+        {
+            foreach (var fi in this.fish)
+            {
+                fi.Eat();
+            }
+        }
+
+        public string GetInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            string fishName = string.Empty;
+            if(this.fish.Count== 0)
+            {
+                fishName = "none";
+            }
+            else
+            {
+                List<string> names=this.fish.Select(fish => fish.Name).ToList();
+                fishName=string.Join(", ",names);
+            }
+
+            sb.AppendLine($"{Name} ({this.GetType().Name}):")
+                .AppendLine($"Fish: {fishName}")
+                .AppendLine($"Decorations: {this.decorations.Count}")
+                .AppendLine($"Comfort: {Comfort}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
