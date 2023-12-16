10d049f93dff35cc592249479c7f47dd3f5d9fb2
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2023-12-16 11:43:51 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/C# OOP Retake Exam - 22 August 2020/01. Structure/Models/Drivers/Entities/Driver.cs
@@ -0,0 +1,54 @@
+﻿using EasterRaces.Models.Cars.Contracts;
+using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Drivers.Entities
+{
+    public class Driver : IDriver
+    {
+        private const int MinSimbolsName = 5;
+        private string name;
+        private ICar car;
+        private int numberOfWins;
+        private bool canParticipate;
+        public Driver(string name)
+        {
+            Name = name;
+            this.car = null;
+           this. canParticipate = false;
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < MinSimbolsName)
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName, value, MinSimbolsName));
+
+                name = value;
+            }
+        }
+        public ICar Car { get => car; }
+
+        public int NumberOfWins { get => numberOfWins; }
+
+        public bool CanParticipate { get => canParticipate; }
+
+        public void AddCar(ICar car)
+        {
+            if (car == null)
+                throw new ArgumentNullException(string.Format(ExceptionMessages.CarInvalid));
+
+            this.car = car;
+            this.canParticipate = true;
+        }
+
+        public void WinRace()
+        {
+            this.numberOfWins++;
+        }
+    }
+}
---
+++ 19 C# OOP Exam/C# OOP Retake Exam - 22 August 2020/01. Structure/Models/Races/Entities/Race.cs
@@ -0,0 +1,61 @@
+﻿using EasterRaces.Models.Drivers.Contracts;
+using EasterRaces.Models.Races.Contracts;
+using EasterRaces.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasterRaces.Models.Races.Entities
+{
+    public class Race : IRace
+    {
+        private const int MinSimbilsName = 5;
+        private const int MinLaps = 1;
+        private string name;
+        private int laps;
+        private List<IDriver> drivers;
+        public Race(string name,int laps)
+        {
+            Name = name;
+            this.drivers=new List<IDriver>();   
+        }
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < MinSimbilsName)
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidName, value, MinSimbilsName));
+
+                name = value;
+            }
+        }
+
+        public int Laps
+        {
+            get => laps;
+            private set
+            {
+                if(value<MinLaps)
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidNumberOfLaps,MinLaps));
+                laps = value;
+            }
+        }
+
+        public IReadOnlyCollection<IDriver> Drivers => this.drivers.AsReadOnly();
+
+        public void AddDriver(IDriver driver)
+        {
+            if (driver == null)
+                throw new ArgumentNullException(string.Format(ExceptionMessages.DriverInvalid));
+
+            if(!driver.CanParticipate)
+                throw new ArgumentException(string.Format(ExceptionMessages.DriverNotParticipate, driver.Name));
+
+            if(this.drivers.Contains(driver))
+                throw new ArgumentNullException(string.Format(ExceptionMessages.DriverAlreadyAdded,driver.Name,Name));
+
+            this.drivers.Add(driver);
+        }
+    }
+}
