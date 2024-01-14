6760e60bbf41ea92fe0dc4ae98968cd0a4b3bc4c
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-01-14 14:12:35 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/09 C# OOP Retake Exam – 22 August 2022/01. Structure/Repositories/BookingRepository.cs
@@ -0,0 +1,28 @@
+﻿using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Repositories.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BookingApp.Repositories
+{
+    public class BookingRepository : IRepository<IBooking>
+    {
+        private List<IBooking> all;
+        public BookingRepository() 
+        { 
+            this.all = new List<IBooking>();
+        }
+        public void AddNew(IBooking model)
+        {
+            this.all.Add(model);
+        }
+
+        public IReadOnlyCollection<IBooking> All() => this.all.AsReadOnly();
+        
+
+        public IBooking Select(string criteria)
+        {
+            return this.all.FirstOrDefault(x=>x.BookingNumber==int.Parse(criteria));
+        }
+    }
+}
