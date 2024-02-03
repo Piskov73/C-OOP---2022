96c2222bb0ac6fa6335c5da2b216952c65bf4a23
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-02-03 19:22:09 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/14 C# OOP Regular Exam - 11 December 2021/01. Structure/Models/Athletes/Boxer.cs
@@ -0,0 +1,26 @@
+﻿using Gym.Utilities.Messages;
+using System;
+
+namespace Gym.Models.Athletes
+{
+    public class Boxer : Athlete
+    {
+        private const int InitialStamina = 60;
+        private const int ExerciseStamina = 15;
+        public Boxer(string fullName, string motivation, int numberOfMedals) 
+            : base(fullName, motivation, numberOfMedals, InitialStamina)
+        {
+        }
+
+        public override void Exercise()
+        {
+            base.Stamina += ExerciseStamina;
+            if (base.Stamina > 100)
+            {
+                base.Stamina = 100;
+
+                throw new ArgumentException(string.Format(ExceptionMessages.InvalidStamina));
+            }
+        }
+    }
+}
---
+++ 19 C# OOP Exam/14 C# OOP Regular Exam - 11 December 2021/01. Structure/Models/Equipment/BoxingGloves.cs
@@ -0,0 +1,12 @@
+﻿namespace Gym.Models.Equipment
+{
+    public class BoxingGloves : Equipment
+    {
+        private const double EquipmentWeight = 227;
+        private const decimal EquipmentPrice = 120;
+        public BoxingGloves()
+            : base(EquipmentWeight, EquipmentPrice)
+        {
+        }
+    }
+}
