a4ef08b2cb01014085651e572ed365e66390c9d1
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-02-08 19:51:32 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/17 C# OOP Retake Exam - 18 April 2021/03. Unit Tests/PresentsTests.cs
@@ -0,0 +1,109 @@
+﻿namespace Presents.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class PresentsTests
+    {
+        private Present present;
+        private Bag bag;
+        [SetUp]
+        public void SetUp()
+        {
+            present = new Present("Name", 20);
+            bag = new Bag();
+        }
+        [Test]
+        public void Test_Present()
+        {
+            Assert.NotNull(present);
+
+            Assert.AreEqual("Name", present.Name);
+            Assert.AreEqual(20, present.Magic);
+        }
+        [Test]
+        public void Test_BagConstructor()
+        {
+            Assert.NotNull(bag);
+        }
+        [Test]
+        public void Test_BagCreate()
+        {
+            string name = "Name";
+            double magic = 100;
+
+            present=new Present(name, magic);
+
+            string expected= $"Successfully added present {name}.";
+            string actual = bag.Create(present);
+
+            Assert.AreEqual(expected, actual);
+
+            Assert.Throws<InvalidOperationException>(()=>bag.Create(present), "This present already exists!");
+
+            present = null;
+            Assert.Throws<ArgumentNullException>(() => bag.Create(present), "Present is null");
+        }
+        [Test]
+        public void Test_BagRemove()
+        {
+            string name = "Name";
+            double magic = 100;
+
+            present = new Present(name, magic);
+
+            bag.Create(present);
+
+            Assert.True(bag.Remove(present));
+            Assert.False(bag.Remove(present));
+        }
+        [Test]
+        public void Test_BagGetPresentWithLeastMagic()
+        {
+            string name = "Name";
+            double magic = 100;
+
+            present = new Present(name, magic);
+
+            bag.Create(present);
+            bag.Create(new Present("KOCHO", 200));
+
+            var pre=bag.GetPresentWithLeastMagic();
+
+            Assert.AreEqual(pre, present);
+            Assert.AreEqual(magic, pre.Magic);
+            Assert.AreEqual(name, pre.Name);
+        }
+        [Test]
+        public void Test_BagGetPresent()
+        {
+            string name = "Name";
+            double magic = 100;
+
+            present = new Present(name, magic);
+
+            bag.Create(present);
+            bag.Create(new Present("KOCHO", 200));
+
+            Assert.NotNull(bag.GetPresent(name));
+            Assert.Null(bag.GetPresent("Vancho"));
+
+        }
+        [Test]
+        public void Test_BagGetPresents()
+        {
+            string name = "Name";
+            double magic = 100;
+
+            present = new Present(name, magic);
+
+            bag.Create(present);
+            bag.Create(new Present("KOCHO", 200));
+
+            var getPresents = bag.GetPresents();
+
+            Assert.AreEqual(2,getPresents.Count);
+        }
+    }
+}
