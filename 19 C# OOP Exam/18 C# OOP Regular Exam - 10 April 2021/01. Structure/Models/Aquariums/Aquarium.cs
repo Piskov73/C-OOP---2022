a6e03fe6fee9d322b5a1baa0daffaaf686e2ad4f
Nenko Piskov <108175657+Piskov73@users.noreply.github.com>
2024-02-10 13:20:33 +0200

    Add files via upload

---
+++ 19 C# OOP Exam/18 C# OOP Regular Exam - 10 April 2021/01. Structure/Models/Aquariums/Aquarium.cs
@@ -0,0 +1,94 @@
+﻿using AquaShop.Models.Aquariums.Contracts;
+using AquaShop.Models.Decorations.Contracts;
+using AquaShop.Models.Fish.Contracts;
+using AquaShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquaShop.Models.Aquariums
+{
+
+    public abstract class Aquarium : IAquarium
+    {
+        private string name;
+        private int capacity;
+        private List<IDecoration> decorations;
+        private List<IFish> fish;
+        public Aquarium(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.decorations = new List<IDecoration>();
+            this.fish = new List<IFish>();
+        }
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException(string.Format(ExceptionMessages.InvalidAquariumName));
+                this.name = value;
+            }
+        }
+        public int Capacity { get => this.capacity; private set => this.capacity = value; }
+
+        public int Comfort => this.decorations.Sum(x => x.Comfort);
+
+        public ICollection<IDecoration> Decorations => this.decorations.AsReadOnly();
+
+        public ICollection<IFish> Fish => this.fish.AsReadOnly();
+        public void AddFish(IFish fish)
+        {
+            if (this.fish.Count == this.Capacity)
+                throw new InvalidOperationException(string.Format(ExceptionMessages.NotEnoughCapacity));
+
+            this.fish.Add(fish);
+        }
+        public bool RemoveFish(IFish fish)
+        {
+            return this.fish.Remove(fish);
+        }
+
+        public void AddDecoration(IDecoration decoration)
+        {
+            this.decorations.Add(decoration);
+        }
+
+
+        public void Feed()
+        {
+            foreach (var fish in this.fish)
+            {
+                fish.Eat();
+            }
+        }
+
+        public string GetInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string fishName = "none";
+            if(this.fish.Count > 0)
+            {
+                List<string> names=new List<string>();
+                foreach (var item in this.fish)
+                {
+                    names.Add(item.Name);
+                }
+
+                fishName = string.Join(", ", names);
+            }
+
+            sb.AppendLine($"{Name} ({this.GetType().Name}):")
+                .AppendLine($"Fish: {fishName}")
+                .AppendLine($"Decorations: {Decorations.Count}")
+                .AppendLine($"Comfort: {Comfort}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ 19 C# OOP Exam/18 C# OOP Regular Exam - 10 April 2021/01. Structure/Models/Fish/FreshwaterFish.cs
@@ -0,0 +1,20 @@
+﻿namespace AquaShop.Models.Fish
+{
+    public class FreshwaterFish : Fish
+    {
+        private const int FISH_SIZE= 3;
+        private const int INCREASES = 3;
+        public FreshwaterFish(string name, string species, decimal price)
+            : base(name, species, price)
+        {
+            base.Size = FISH_SIZE;
+        }
+
+       
+
+        public override void Eat()
+        {
+            base.Size+=INCREASES;
+        }
+    }
+}
